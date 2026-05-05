0d85d2430945b1c2f848f2c17cd82965f489bd94
brunof23 <brunoferreira.tecnico@gmail.com>
2026-05-05 20:22:14 -0300

    feat: estrutura inicial do projeto

---
+++ fase1/src/test/java/com/fiap/fase1/Fase1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.fase1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fase1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
